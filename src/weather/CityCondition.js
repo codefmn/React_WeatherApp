f5dd24494c211dff3f6417be66ed6b6eba10e3b4
mfxy <mfxyzm@outlook.com>
2018-05-26 08:26:37 +1000

    Update CityCondition

--- src/weather/CityCondition.js
+++ src/weather/CityCondition.js
@@ -4,9 +4,9 @@ export default function CityCondition(props){
     const {city, weather, temp} = props;
     return (
         <ul>
-            <li id="location">{props.city}</li>
-            <li id="weather">{props.weather}</li>
-            <li id="temperature">{props.temp}</li>
+            <li id="location">{city}</li>
+            <li id="weather">{weather}</li>
+            <li id="temperature">{temp}</li>
          </ul>
        )
 }
