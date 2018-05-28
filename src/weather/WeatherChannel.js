d93d5a5efb23c4d5fba0152167b7a1b236112934
mfxy <mfxyzm@outlook.com>
2018-05-28 19:15:29 +1000

    Add fetch data from api

--- src/weather/WeatherChannel.js
+++ src/weather/WeatherChannel.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 import CityCondition from './CityCondition';
 import Forecaster    from './Forecaster';
+import { fetchConditionData, fetchForecast } from '../api/weather';
 
 // responsible for maintain necessary data (from API response) in the state
 // pass them down to child 
@@ -10,15 +11,16 @@ export default class WeatherChannel extends Component {
         super(props);
         // use static data to fill initial state first
         this.state = {
+            currentCity:"Brisbane",
             condition: {
-                city:  'Brisbane, Au',
-                temp: '12c',
-                weather: 'Clear'
+                // city:  'Brisbane, Au',
+                // temp: '12c',
+                // weather: 'Clear'
             },
-            days:  [
-                {weekday: 'Wed', high:23, low:18, icon:'http://icons.wxug.com/i/c/k/clear.gif'},
-                {weekday: 'Thu', high:29, low:18, icon:'http://icons.wxug.com/i/c/k/chancerain.gif'},
-                {weekday: 'Fri', high:20, low:10, icon:'http://icons.wxug.com/i/c/k/chancerain.gif'}
+            forecast:  [
+                // {weekday: 'Wed', high:23, low:18, icon:'http://icons.wxug.com/i/c/k/clear.gif'},
+                // {weekday: 'Thu', high:29, low:18, icon:'http://icons.wxug.com/i/c/k/chancerain.gif'},
+                // {weekday: 'Fri', high:20, low:10, icon:'http://icons.wxug.com/i/c/k/chancerain.gif'}
             ]
         }
     }
@@ -33,9 +35,41 @@ export default class WeatherChannel extends Component {
             weather={this.state.condition.weather} />
           </section>
           <section id="right">
-            <Forecaster days={this.state.days}/>
+            <Forecaster forecast={this.state.forecast}/>
           </section>
         </main>
       )
+    }
+
+    // handleCondition(data){
+    //   console.log(data);
+    //   const condition ={
+    //     city: data.display_location.full,
+    //     weather: data.weather,
+    //     temp: data.temp_c
+    //   }
+    //   this.setState({condition});
+    // }
+
+    componentDidMount(){
+      fetchConditionData(this.state.currentCity,(data)=>{
+        const condition ={
+          city: data.display_location.full,
+          weather: data.weather,
+          temp: data.temp_c
+        }
+        this.setState({condition});
+    });
+      fetchForecast(this.state.currentCity,(forecast)=>{
+        const data = forecast.map(item=>{
+          return{
+            weekday: item.weekday_short,
+            high: item.high.celsius,
+            low: item.low.celsius,
+            icon: item.icon_url
+          }
+        })
+        this.setState({forecast:data});
+      });
     }
 }
